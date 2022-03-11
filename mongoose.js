616c7e4060a7a7818559b28bdc70ed92929d622e
Shubhrant05 <shubhrantyadav34366@gmail.com>
2022-03-11 15:58:50 +0530

    changes in mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -5,7 +5,7 @@ const { findById } = require('./models/Shop-model');
 const Shop = require('./models/Shop-model')
 const User = require('./models/User-model')
 
-const connString = "mongodb+srv://shubhrant:luucent0@cluster0.feny1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connString = "mongodb+srv://shubhrant:luucent0@cluster0.feny1.mongodb.net/shop_aisle?retryWrites=true&w=majority"
 const connString1 ='mongodb://localhost:27017/shop_aisle?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
 mongoose.connect(
  connString
